bd59b6efdfd7315c58ffa034b7e1099c76d33d86
Cristian-Serrano <gcristiag@gmail.com>
2024-03-12 12:44:26 +0100

    usuario service

--- src/main/java/org/iesvdm/ecotrack/service/UsuarioService.java
+++ src/main/java/org/iesvdm/ecotrack/service/UsuarioService.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -24,6 +25,26 @@ public class UsuarioService {
 
     public Optional<Usuario> one(long id){
         return usuarioRepository.findById(id);
+    }
+
+    public Usuario replace(Long id, Usuario usuario){
+        return this.usuarioRepository.findById(id)
+                .map( p -> (id.equals(usuario.getId())  ?
+                        this.usuarioRepository.save(usuario) : null))
+                .get();
+                /*.orElseThrow(() -> new UsuarioNotFoundException(id));*/
+    }
+
+    public void delete(Long id) {
+        this.usuarioRepository.findById(id).map(p -> {
+                    this.usuarioRepository.delete(p);
+                    return p;})
+                .get();
+                /*.orElseThrow(() -> new UsuarioNotFoundException(id));*/
+    }
+
+    public Usuario save(Usuario usuario){
+        return this.usuarioRepository.save(usuario);
     }
 
 }
